ec2e0c3d2834a36564e839ae5c5cdc2473de93dc
Nikolay Stashulyov <stashulyov.nikolay@gmail.com>
2020-06-19 00:46:57 +0300

    Add tests for ActionNode

--- Assets/Scripts/Engine/Nodes/SequenceNode.cs
+++ Assets/Scripts/Engine/Nodes/SequenceNode.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Engine
@@ -6,6 +7,21 @@ namespace Engine
     [CreateAssetMenu(menuName = "SOBehaviourTree/Engine/SequenceNode", fileName = "SequenceNode")]
     public class SequenceNode : Node
     {
-        public List<Node> Children;
+        public List<Node> Children = new List<Node>();
+
+        public override void OnInitialize()
+        {
+            if (Children == null)
+                throw new NullReferenceException();
+
+            foreach (var item in Children)
+            {
+                if (item == null)
+                    throw new NullReferenceException();
+            }
+
+            foreach (var item in Children)
+                item.OnInitialize();
+        }
     }
 }
--- Assets/Scripts/Tests/ActionNodeTests.cs
+++ Assets/Scripts/Tests/ActionNodeTests.cs
@@ -1,10 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
 using Engine;
+using NSubstitute;
 using NUnit.Framework;
 using UnityEngine;
 
 namespace Tests
 {
+    public class SequenceNodeTests
+    {
+        [Test]
+        public void OnInitialize_ChildrenListIsNull_Throws()
+        {
+            var node = ScriptableObject.CreateInstance<SequenceNode>();
+            node.Children = null;
+
+            void Act() => node.OnInitialize();
+
+            Assert.Throws<NullReferenceException>(Act);
+        }
+
+        [Test]
+        public void OnInitialize_OneOfChildrenIsNull_Throws()
+        {
+            var node = ScriptableObject.CreateInstance<SequenceNode>();
+            node.Children = new List<Node> {null};
+
+            void Act() => node.OnInitialize();
+
+            Assert.Throws<NullReferenceException>(Act);
+        }
+
+        [Test]
+        public void OnInitialize_SeveralChildren_CallsEach()
+        {
+            var node = ScriptableObject.CreateInstance<SequenceNode>();
+            var child1 = Substitute.For<Node>();
+            var child2 = Substitute.For<Node>();
+            node.Children = new List<Node> {child1, child2};
+
+            node.OnInitialize();
+
+            child1.Received().OnInitialize();
+            child2.Received().OnInitialize();
+        }
+    }
+
     public class ActionNodeTests
     {
         [Test]
@@ -63,7 +104,7 @@ namespace Tests
             {
                 ReceivedOnInitialize = true;
             }
-            
+
             public override void OnExecute()
             {
                 ReceivedOnExecute = true;
