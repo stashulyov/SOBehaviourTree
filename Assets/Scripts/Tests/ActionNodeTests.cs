02dec7b9600e6f57d89ba528222da1a32f3128df
Nikolay Stashulyov <stashulyov.nikolay@gmail.com>
2020-06-19 00:47:44 +0300

    Refactor tests

--- Assets/Scripts/Tests/ActionNodeTests.cs
+++ Assets/Scripts/Tests/ActionNodeTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using Engine;
 using NSubstitute;
 using NUnit.Framework;
@@ -7,45 +6,6 @@ using UnityEngine;
 
 namespace Tests
 {
-    public class SequenceNodeTests
-    {
-        [Test]
-        public void OnInitialize_ChildrenListIsNull_Throws()
-        {
-            var node = ScriptableObject.CreateInstance<SequenceNode>();
-            node.Children = null;
-
-            void Act() => node.OnInitialize();
-
-            Assert.Throws<NullReferenceException>(Act);
-        }
-
-        [Test]
-        public void OnInitialize_OneOfChildrenIsNull_Throws()
-        {
-            var node = ScriptableObject.CreateInstance<SequenceNode>();
-            node.Children = new List<Node> {null};
-
-            void Act() => node.OnInitialize();
-
-            Assert.Throws<NullReferenceException>(Act);
-        }
-
-        [Test]
-        public void OnInitialize_SeveralChildren_CallsEach()
-        {
-            var node = ScriptableObject.CreateInstance<SequenceNode>();
-            var child1 = Substitute.For<Node>();
-            var child2 = Substitute.For<Node>();
-            node.Children = new List<Node> {child1, child2};
-
-            node.OnInitialize();
-
-            child1.Received().OnInitialize();
-            child2.Received().OnInitialize();
-        }
-    }
-
     public class ActionNodeTests
     {
         [Test]
@@ -62,59 +22,36 @@ namespace Tests
         public void OnInitialize_CallsActionTask()
         {
             var node = ScriptableObject.CreateInstance<ActionNode>();
-            var actionTask = ScriptableObject.CreateInstance<ActionTaskMock>();
+            var actionTask = Substitute.For<ActionTask>();
             node.ActionTask = actionTask;
 
             node.OnInitialize();
 
-            Assert.IsTrue(actionTask.ReceivedOnInitialize);
+            actionTask.Received().OnInitialize();
         }
 
         [Test]
         public void OnExecute_CallsActionTask()
         {
             var node = ScriptableObject.CreateInstance<ActionNode>();
-            var actionTask = ScriptableObject.CreateInstance<ActionTaskMock>();
+            var actionTask = Substitute.For<ActionTask>();
             node.ActionTask = actionTask;
 
             node.OnExecute();
 
-            Assert.IsTrue(actionTask.ReceivedOnExecute);
+            actionTask.Received().OnExecute();
         }
 
         [Test]
         public void OnUpdate_CallsActionTask()
         {
             var node = ScriptableObject.CreateInstance<ActionNode>();
-            var actionTask = ScriptableObject.CreateInstance<ActionTaskMock>();
+            var actionTask = Substitute.For<ActionTask>();
             node.ActionTask = actionTask;
 
             node.OnUpdate(1f);
 
-            Assert.IsTrue(actionTask.ReceivedOnUpdate);
-        }
-
-        private class ActionTaskMock : ActionTask
-        {
-            public bool ReceivedOnExecute;
-            public bool ReceivedOnUpdate;
-            public bool ReceivedOnInitialize;
-
-            public override void OnInitialize()
-            {
-                ReceivedOnInitialize = true;
-            }
-
-            public override void OnExecute()
-            {
-                ReceivedOnExecute = true;
-            }
-
-            public override Result OnUpdate(float deltaTime)
-            {
-                ReceivedOnUpdate = true;
-                return Result.Success;
-            }
+            actionTask.ReceivedWithAnyArgs().OnUpdate(default);
         }
     }
 }
---
+++ Assets/Scripts/Tests/SequenceNodeTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Engine;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class SequenceNodeTests
+    {
+        [Test]
+        public void OnInitialize_ChildrenListIsNull_Throws()
+        {
+            var node = ScriptableObject.CreateInstance<SequenceNode>();
+            node.Children = null;
+
+            void Act() => node.OnInitialize();
+
+            Assert.Throws<NullReferenceException>(Act);
+        }
+
+        [Test]
+        public void OnInitialize_OneOfChildrenIsNull_Throws()
+        {
+            var node = ScriptableObject.CreateInstance<SequenceNode>();
+            node.Children = new List<Node> {null};
+
+            void Act() => node.OnInitialize();
+
+            Assert.Throws<NullReferenceException>(Act);
+        }
+
+        [Test]
+        public void OnInitialize_SeveralChildren_CallsEach()
+        {
+            var node = ScriptableObject.CreateInstance<SequenceNode>();
+            var child1 = Substitute.For<Node>();
+            var child2 = Substitute.For<Node>();
+            node.Children = new List<Node> {child1, child2};
+
+            node.OnInitialize();
+
+            child1.Received().OnInitialize();
+            child2.Received().OnInitialize();
+        }
+    }
+}
